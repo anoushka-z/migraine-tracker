08570f2778855e663488167ef99f2434066b871c
anoushka <anoushka.shresth@zomato.com>
2026-05-07 17:25:47 +0530

    Initial Commit

---
+++ migraine tracket/migraine_tracketApp.swift
@@ -0,0 +1,17 @@
+//
+//  migraine_tracketApp.swift
+//  migraine tracket
+//
+//  Created by Anoushka Shresth on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct migraine_tracketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
